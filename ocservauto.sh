dbef2da1d9c1bef5b3fe9c187cdfe779cb6124c6
Ramik0 <Ramik0>
2019-11-24 16:24:00 +0800

    fix display bug

--- ocservauto.sh
+++ ocservauto.sh
@@ -1579,7 +1579,7 @@ else
 	echo -e " 当前状态: ${Red_font_prefix}未安装${Font_color_suffix}"
 fi
 echo
-read -e -p " 请输入数字 [0-14]:" num
+read -e -p " 请输入数字 [0-16]:" num
 case "$num" in
 	0)
 	Update_Shell
